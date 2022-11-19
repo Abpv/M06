a633057a743d198699fa061049f31a98cf5deae0
Abpv <74953874+Abpv@users.noreply.github.com>
2022-11-19 16:16:59 +0100

    inicial

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -1,4 +1,12 @@
 // Este array no se puede modificar,
 var posibilidades = ["piedra", "papel", "tijera"];
+const btnJugar = document.getElementsByTagName('button')[0];
+const btnYa = document.getElementsByTagName('button')[1];
+const btnreset = document.getElementsByTagName('button')[2];
+
+btnJugar.addEventListener("click", iniciarPartida)
 //    //
 
+function iniciarPartida(){
+    console.log("iniciando partida")
+}
